9adc0e60e18608324915abfe80b4fc446d9719e3
EI1ot <119261308+eliot17white@users.noreply.github.com>
2023-01-06 20:58:37 +0000

    Removed unneeded dependencies

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (request, response) => {
 
 fs.readdir('./commands/', async (err, files) => {
     if(err){
-        return console.log(chalk.red('An error occured when checking the commands folder for commands to load: ' + err));
+        return console.log('An error occured when checking the commands folder for commands to load: ' + err);
     }
   
     files.forEach(async (file) => {
@@ -39,7 +39,7 @@ fs.readdir('./commands/', async (err, files) => {
 
 fs.readdir('./modules/', async (err, files) => {
     if(err){
-        return console.log(chalk.red('An error occured when checking the modules folder for modules to load: ' + err));
+        return console.log('An error occured when checking the modules folder for modules to load: ' + err);
     }
     files.forEach(async (file) => {
         if(!file.endsWith('.js')) return;
